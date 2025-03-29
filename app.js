672c24bfa70f0082d67de6081e2326d70cacdd4d
Azalia-02 <azalia.mejiav@gmail.com>
2025-03-29 11:33:43 -0600

    cambios

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
+const cors = require('cors');   
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 
 app.use(bodyParser.json());
+app.use(cors({
+    origin: '*', 
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization'
+  }));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -14,7 +21,7 @@ app.use((req, res, next) => {
 
 app.use('/api', routes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
     console.log(`Servidor API a la espera de consulta, por el puerto ${PORT}`);
 });
